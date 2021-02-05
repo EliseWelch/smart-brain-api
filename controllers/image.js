dd8e0188aa2c3c16d845691342a59fec6675ad36
Elise Welch <eliseanyawelch@gmail.com>
2021-02-05 14:53:16 -0500

    smart brain server

---
+++ controllers/image.js
@@ -0,0 +1,31 @@
+const Clarifai = require('clarifai');
+
+const app = new Clarifai.App({
+    apiKey: 'b145dce7256d49dbbd0ddb2d239a71e6'
+});
+
+const handleApiCall = (req, res) => {
+    app.models
+        .predict(Clarifai.FACE_DETECT_MODEL, req.body.input)
+        .then(data => {
+            res.json(data);
+        })
+        .catch(err => res.status(400).json('Unable to work with API'))
+}
+
+
+const handleImage = (db) => (req, res) => {
+    const { id } = req.body;
+    db('users').where('id', '=', id)
+    .increment('entries', 1)
+    .returning('entries')
+    .then(entries => { 
+        res.json(entries[0]);
+    })
+    .catch(err => res.status(400).json('Unable to load entries'))
+}
+
+module.exports = {
+    handleImage: handleImage,
+    handleApiCall: handleApiCall
+};
